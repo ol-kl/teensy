046395dbb163e972ea8952dbd9bb89d958a0036d
ol-kl <ol-kl@users.noreply.github.com>
2015-08-09 06:08:39 +0100

    Update tiny5.asm
    First Integer argument for a function call must be placed into RDI register on IA64 SystemV platform

--- tiny5.asm
+++ tiny5.asm
@@ -1,7 +1,12 @@
+; nasm -f elf64 tiny.asm
+; gcc -Wall -nostartfiles -O3 -s tiny.o
 bits 64
 global _start
 extern _exit
 section .text
 _start:
-    push dword 42
+    mov rdi,42 ;System V AMD64 ABI calling conventions
+               ;differ from IA32. Leaving orig version
+               ;results in returing 200 (or other garbage from EAX)
+               ;instead of 42
     call _exit
